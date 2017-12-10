1cad27b48cf4f35a0ac9b1eed3fde2ff1eb97f95
asorathiya <sorathiya.ankit@gmail.com>
2017-12-10 13:28:32 +0530

    adding boolean expressin satisfiablity

--- src/main/java/com/engineeralgorithmanddrdatastructure/npproblems/sat/BooleanSatisfiability.java
+++ src/main/java/com/engineeralgorithmanddrdatastructure/npproblems/sat/BooleanSatisfiability.java
@@ -1,5 +1,39 @@
 package com.engineeralgorithmanddrdatastructure.npproblems.sat;
 
+/**
+ * 
+ * @author ankit.sorathiya
+ *
+ *         Given variables and their assigned values find out whether given
+ *         expression satisfies to true or not?
+ * 
+ *         expression is conjunction of clauses
+ * 
+ *         eg. clauses[0] && clauses[1]..... &&clauses[n-1]
+ * 
+ *         each variable is assigned initial values which could either be true
+ *         of false.
+ * 
+ *         Expression: { { 1, 2, -3 }, { -1, 2, -3 } }
+ * 
+ *         Assignment : { false,true, false, false }, 0th index is fake since we
+ *         can not represent -0:)
+ * 
+ *         output=> true
+ * 
+ *         Explanation:
+ * 
+ *         first clause can be true using 1 or -3 hence result into true
+ * 
+ * 
+ *         second clause can be true using -3 hence result into true
+ * 
+ *         since both clauses return true and the expression is conjunction of
+ *         clauses it returns true. If any of the clause returns false your
+ *         expression can never satisfy.
+ * 
+ * 
+ */
 public class BooleanSatisfiability {
 	public static boolean isBooleanExpressionSatisfiable(int[][] clauses, boolean[] variableAssignments) {
 		for (int index = 0; index < clauses.length; index++) {
