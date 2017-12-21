51174b65de352d2398614a1e7ec19903d9b4798e
asorathiya <sorathiya.ankit@gmail.com>
2017-12-21 13:41:04 +0530

    yet to add unit test cases for robot home treassure

--- src/main/java/com/engineeralgorithmanddrdatastructure/dp/RobotHomeProblem.java
+++ src/main/java/com/engineeralgorithmanddrdatastructure/dp/RobotHomeProblem.java
@@ -2,21 +2,25 @@ package com.engineeralgorithmanddrdatastructure.dp;
 
 public class RobotHomeProblem {
 	public int howManyWaysRobotCanReachHome(int[][] maze, int homeRow, int homeColumn) {
-		int[][] memo = new int[maze.length][maze.length];
-		initializeBaseCase(memo);
+		int[][] memo = initializeMemo(maze);
 		for (int row = memo.length - 2; row >= 0; row--) {
 			for (int col = memo.length - 2; col >= 0; col--) {
-				memo[row][col] = memo[row][col + 1] + memo[row + 1][col];
+				if (maze[row][col] == 0) {
+					memo[row][col] = 0;
+				} else {
+					memo[row][col] = memo[row][col + 1] + memo[row + 1][col];
+				}
 			}
 		}
 		return memo[0][0];
 	}
 
-	private int[][] initializeBaseCase(int[][] maze) {
+	private int[][] initializeMemo(int[][] maze) {
 		int[][] memo = new int[maze.length][maze.length];
-		for (int index = 0; index < maze.length - 2; index++) {
-			memo[index][maze.length - 1] = 1;
-			memo[maze.length - 1][index] = 1;
+		memo[maze.length - 1][maze.length - 1] = 1;
+		for (int index = maze.length - 2; index >= 0; index--) {
+			memo[index][maze.length - 1] = maze[index][maze.length - 1] == 0 ? 0 : maze[index + 1][maze.length - 1];
+			memo[maze.length - 1][index] = maze[maze.length - 1][index] == 0 ? 0 : maze[maze.length - 1][index + 1];
 		}
 		return memo;
 	}
