f43e800e2f3bb4603776cb161962d2ccf6a6891f
asorathiya <sorathiya.ankit@gmail.com>
2018-01-04 14:01:31 +0530

    adding product of integer array except element at given index

---
+++ src/main/java/com/engineeralgorithmanddrdatastructure/array/ProductNumber.java
@@ -0,0 +1,49 @@
+package com.engineeralgorithmanddrdatastructure.array;
+
+/**
+ * 
+ * @author ankit.sorathiya
+ *
+ *
+ *         You have an array of integers, and for each index you want to find
+ *         the product of every integer except the integer at that index.
+ */
+public class ProductNumber {
+	public static int[] findProduct(int[] data) {
+		if (data == null || data.length < 2) {
+			return null;
+		}
+		int[] result = new int[data.length];
+		int zeroIndex = -1;
+		int zeroCount = 0;
+		int product = 1;
+		for (int index = 0; index < data.length; index++) {
+			if (data[index] == 0) {
+				zeroCount++;
+				zeroIndex = index;
+			}
+			if (zeroCount > 1) {
+				product = 0;
+				break;
+			} else if (data[index] != 0) {
+				product *= data[index];
+			}
+		}
+		if (zeroCount > 1) {
+			return result;
+		}
+
+		for (int index = 0; index < data.length; index++) {
+			if (data[index] != 0) {
+				if (zeroCount == 1 && zeroIndex != index) {
+					result[index] = 0;
+				} else {
+					result[index] = product / data[index];
+				}
+			} else {
+				result[index] = product;
+			}
+		}
+		return result;
+	}
+}
---
+++ src/test/java/com/engineeralgorithmanddrdatastructure/array/ProductNumberTest.java
@@ -0,0 +1,17 @@
+package com.engineeralgorithmanddrdatastructure.array;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class ProductNumberTest {
+	@Test
+	public void testProductNumbers() {
+		assertArrayEquals(null, ProductNumber.findProduct(null));
+		assertArrayEquals(null, ProductNumber.findProduct(new int[] { 0 }));
+		assertArrayEquals(new int[] { 1, 0 }, ProductNumber.findProduct(new int[] { 0, 1 }));
+		assertArrayEquals(new int[] { 0, 0, 0, 0 }, ProductNumber.findProduct(new int[] { 0, 2, 3, 0 }));
+		assertArrayEquals(new int[] { 0, 0, 6 }, ProductNumber.findProduct(new int[] { 2, 3, 0 }));
+		assertArrayEquals(new int[] { 18, 12, 6 }, ProductNumber.findProduct(new int[] { 2, 3, 6 }));
+	}
+}
