605f49345878a94a6d2aaefd89e19b17d9ef8f4e
asorathiya <sorathiya.ankit@gmail.com>
2018-01-06 16:21:47 +0530

    changed the test class name

---
+++ src/test/java/com/engineeralgorithmanddrdatastructure/dp/MagicalStringProblemTest.java
@@ -0,0 +1,16 @@
+package com.engineeralgorithmanddrdatastructure.dp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MagicalStringProblemTest {
+	@Test
+	public void testWordCount() {
+		assertEquals(0, MagicalStringProblem.countMagicalStringOfLength(0));
+		assertEquals(5, MagicalStringProblem.countMagicalStringOfLength(1));
+		assertEquals(10, MagicalStringProblem.countMagicalStringOfLength(2));
+		assertEquals(19, MagicalStringProblem.countMagicalStringOfLength(3));
+		assertEquals(30, MagicalStringProblem.countMagicalStringOfLength(4));
+	}
+}
