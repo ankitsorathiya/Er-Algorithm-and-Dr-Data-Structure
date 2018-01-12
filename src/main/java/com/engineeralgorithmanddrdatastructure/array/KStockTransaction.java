c67ce2b0cf69898b848087729404de03861db1ca
asorathiya <sorathiya.ankit@gmail.com>
2018-01-12 12:58:53 +0530

    adding kstock transaction new solution

--- src/main/java/com/engineeralgorithmanddrdatastructure/array/KStockTransaction.java
+++ src/main/java/com/engineeralgorithmanddrdatastructure/array/KStockTransaction.java
@@ -1,6 +1,8 @@
 package com.engineeralgorithmanddrdatastructure.array;
 
+import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class KStockTransaction {
 	public static int findMaxProfitWithKStockTransaction(int[] stockPrices, int k) {
@@ -31,6 +33,41 @@ public class KStockTransaction {
 		}
 
 		return maxProfit;
+	}
+
+	public static int findMaxProfitWithKStockTransaction_Kadem(int[] stockPrices, int k) {
+		int maxProfit = 0;
+		if (k <= 0 || stockPrices == null || stockPrices.length == 0 || stockPrices.length <= k) {
+			return maxProfit;
+		}
+		Set<Integer> BestBuyIndexes = new HashSet<>();
+		for (int kThTransaction = 1; kThTransaction <= k; kThTransaction++) {
+			int currentBuyIndex = findBestBuyIndex(BestBuyIndexes, stockPrices);
+			int bestBuy = stockPrices[currentBuyIndex];
+			int currentProfit = 0;
+			for (int stockTime = currentBuyIndex + 1; stockTime < stockPrices.length; stockTime++) {
+				if (stockPrices[stockTime] > bestBuy) {
+					currentProfit = Math.max(currentProfit, stockPrices[stockTime] - bestBuy);
+				} else if (!BestBuyIndexes.contains(stockTime)) {
+					bestBuy = stockPrices[stockTime];
+					currentBuyIndex = stockTime;
+				}
+			}
+			BestBuyIndexes.add(currentBuyIndex);
+			maxProfit += currentProfit;
+
+		}
+
+		return maxProfit;
+	}
+
+	private static int findBestBuyIndex(Set<Integer> bestBuyIndexes, int[] stockPrices) {
+		for (int stockTime = 0; stockTime < stockPrices.length; stockTime++) {
+			if (!bestBuyIndexes.contains(stockTime)) {
+				return stockTime;
+			}
+		}
+		return 0;
 	}
 
 }
--- src/test/java/com/engineeralgorithmanddrdatastructure/array/KStockTransactionTest.java
+++ src/test/java/com/engineeralgorithmanddrdatastructure/array/KStockTransactionTest.java
@@ -19,4 +19,19 @@ public class KStockTransactionTest {
 		assertEquals(0,
 				KStockTransaction.findMaxProfitWithKStockTransaction(new int[] { 89, 20, 5, 4, 3, 2, 1 }, 3));
 	}
+
+	@Test
+	public void testKTransactionProfit_kadem() {
+		assertEquals(0, KStockTransaction.findMaxProfitWithKStockTransaction_Kadem(null, 5));
+		assertEquals(0, KStockTransaction.findMaxProfitWithKStockTransaction_Kadem(new int[] {}, 5));
+		assertEquals(0, KStockTransaction.findMaxProfitWithKStockTransaction_Kadem(new int[] { 1, 2 }, 0));
+		assertEquals(0, KStockTransaction.findMaxProfitWithKStockTransaction_Kadem(new int[] { 9, 5, 10, 25, 30 }, 0));
+		assertEquals(66, KStockTransaction.findMaxProfitWithKStockTransaction_Kadem(new int[] { 9, 5, 10, 25, 30 }, 3));
+		assertEquals(145, KStockTransaction.findMaxProfitWithKStockTransaction_Kadem(
+				new int[] { 1, 1, 1, 2, 1, 1, 1, 1, 9, 8, 8, 1, 1, 1, 1, 1, 1, 9, 5, 10, 25, 30 }, 5));
+		assertEquals(2539,
+				KStockTransaction.findMaxProfitWithKStockTransaction_Kadem(new int[] { 30, 20, 33, 56, 89, 874 }, 3));
+		assertEquals(0,
+				KStockTransaction.findMaxProfitWithKStockTransaction_Kadem(new int[] { 89, 20, 5, 4, 3, 2, 1 }, 3));
+	}
 }
